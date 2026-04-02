48aa0f7f0e28eb609b34e2180b761ec0dd025620
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2026-04-02 17:47:29 +0200

    bash: move PS1 space to the end

--- .config/matugen/bash_ps1.sh
+++ .config/matugen/bash_ps1.sh
@@ -3,4 +3,4 @@ R=$((16#${PRIMARY_HEX:1:2}))
 G=$((16#${PRIMARY_HEX:3:2}))
 B=$((16#${PRIMARY_HEX:5:2}))
 
-export PS1="\[\e[38;2;${R};${G};${B}m\][\h][\w]\$ \[\e[0m\]"
+export PS1="\[\e[38;2;${R};${G};${B}m\][\h][\w]\$\[\e[0m\] "
