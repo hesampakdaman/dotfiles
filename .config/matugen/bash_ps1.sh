ff6d13aca9386a49c2ccb71fd96488a7e31559c8
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2026-04-09 22:04:55 +0200

    feat: literate dotfiles.org

--- .config/matugen/bash_ps1.sh
+++
@@ -1,6 +0,0 @@
-PRIMARY_HEX="{{colors.primary.default.hex}}"
-R=$((16#${PRIMARY_HEX:1:2}))
-G=$((16#${PRIMARY_HEX:3:2}))
-B=$((16#${PRIMARY_HEX:5:2}))
-
-export PS1="\[\e[38;2;${R};${G};${B}m\][\h][\w]\$\[\e[0m\] "
--- .config/matugen/emacs.el
+++
@@ -1,8 +0,0 @@
-;; -*- lexical-binding: t; -*-
-;; -*- lexical-binding: t; -*-
-(hesam-matugen-pick-ef-theme-by-hsl
- :primary "{{colors.primary.default.hsl}}"
- :secondary "{{colors.secondary.default.hsl}}"
- :tertiary "{{colors.tertiary.default.hsl}}"
- :surface "{{colors.surface.default.hsl}}"
- :surface-variant "{{colors.surface_variant.default.hsl}}")
